a3a43d190d8e598842976d3466421f62d6d8a66e
Keiquan Blackmon <120697653+Yepitskeys@users.noreply.github.com>
2023-07-09 17:24:42 -0400

    created framework for writetoFile function and initilization function

--- index.js
+++ index.js
@@ -2,16 +2,19 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+// Imports classes from shapes.js
+const Shapes = require('../shapes.js');
+
 const questions = [
     {
       type: 'input',
-      message: 'Please enter the text you would like to add to your logo!',
+      message: 'What text you would like to add to your logo? (Three characters max allowed)',
       name: 'text',
     },
     {
         type: 'checkbox',
-        message: 'What color would you like to make your text?',
-        name: 'color',
+        message: 'What color would you like to make your TEXT?',
+        name: 'textColor',
         choices: [
             {name: 'white', value: 'white'},
             {name: 'black', value: 'black'},
@@ -25,27 +28,56 @@ const questions = [
         ]
       },
       {
-        type: 'checkbox',
+        type: 'list',
         message: 'What shape would you like for your logo?',
         name: 'shape',
+        choices: ["Circle", "Triangle", "Square"],
+      },
+      {
+        type: 'checkbox',
+        message: 'What color would you like to make your SHAPE?',
+        name: 'shapeColor',
         choices: [
-            {name: 'circle', value: 'circle'},
-            {name: 'triangle', value: 'triangle'},
-            {name: 'square', value: 'square'},
+            {name: 'white', value: 'white'},
+            {name: 'black', value: 'black'},
+            {name: 'red', value: 'red'},
+            {name: 'orange', value: 'orange'},
+            {name: 'yellow', value: 'yellow'},
+            {name: 'green', value: 'green'},
+            {name: 'blue', value: 'blue'},
+            {name: 'indigo', value: 'indigo'},
+            {name: 'violet', value: 'violet'},
         ]
       },
-];
+]
+
+// Error if logo text is more than 3 characters
+.then((answers) => {
+    if (answers.text.length > 3) {
+        console.log("Please no more than 3 characters for your text!");
+        questions();
+    } else {
+        writeToFile('logo.svg', answers);
+    }
+})
 
 // Function to write svg file
-function writeToSVG ()
+function writeToFile () {
+    let svgContainer = '';
+
+    // Create Shape Parameters
+    let shapeOptions;
+    if (answers.shape === triangles)
+
+}
 
 // Function to initialize app
 function init() {
   console.log('Welcome to My Logo Generator!\nPlease answer a few quick questions to generate your very own logo!');
 
   inquirer.prompt(questions)
-  .then( {
-      writeToFile
+  .then((answers) => {
+      writeToFile('logo.svg', answers);
   })
 
 };
