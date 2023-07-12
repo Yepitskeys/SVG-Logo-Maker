70554602ba3e3e316860e933321c871c05a08f8f
Keiquan Blackmon <120697653+Yepitskeys@users.noreply.github.com>
2023-07-12 18:13:45 -0400

    starting writeToFile function

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // Imports classes from shapes.js
-const Shapes = require('../shapes.js');
+const Shapes = require('./lib/shapes');
+const {Shape, Triangle, Square, Circle} = require('./lib/shapes.js');
 
-const questions = [
+const questions = ([
     {
       type: 'input',
       message: 'What text you would like to add to your logo? (Three characters max allowed)',
@@ -49,9 +50,7 @@ const questions = [
             {name: 'violet', value: 'violet'},
         ]
       },
-]
-
-// Error if logo text is more than 3 characters
+])
 .then((answers) => {
     if (answers.text.length > 3) {
         console.log("Please no more than 3 characters for your text!");
@@ -59,16 +58,22 @@ const questions = [
     } else {
         writeToFile('logo.svg', answers);
     }
-})
+});
+
+// Error if logo text is more than 3 character
 
 // Function to write svg file
-function writeToFile () {
+function writeToFile (fileName, answers) {
     let svgContainer = '';
+    svgContainer = '<svg version="1.1" width="300" height="200">';
 
     // Create Shape Parameters
     let shapeOptions;
-    if (answers.shape === triangles)
-    
+    if (answers.shape === "Triangle") {
+        shapeOptions = new Triangle();
+        // Need to combine to svgContainer and the parameters pulled in from Triangle
+        svgContainer = `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeColor}" />`
+    }
 
 }
 
